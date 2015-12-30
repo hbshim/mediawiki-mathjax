953dbbb6c2e9eeeef0ea9dcc5daac93535cf6a5a
hbshim <hbshim@gmail.com>
2015-12-30 12:27:31 -0500

    added comment explaining why MarkerVal should be retained / some sugaring to improve redability

--- MathJax.php
+++ MathJax.php
@@ -2,29 +2,33 @@
 if(!defined('MEDIAWIKI'))
 die('This is a mediawiki extensions and can\'t be run from the command line.');
 
-$wgHooks['ParserFirstCallInit'][] = 'MathJax_Parser::RunMathJax'; // register <source>(!) tags and register other hooks
+$wgHooks['ParserFirstCallInit'][] = 'MathJax_Parser::RunMathJax';
 
 class MathJax_Parser {
      
-    static $Markers; ///< Variables for stripping formula's in stage 2
-    static $mark_n = 0; ///< Variables for numbering and resolving references at the end of stage 2 
+    static $Markers; 
+    static $mark_n = 0;
     static $tempScript;
 
     static function RunMathJax(Parser $parser)
     {
         $parser->setHook( 'nomathjax' , 'MathJax_Parser::NoMathJax' );
+
         global $wgHooks;
-        $wgHooks['ParserBeforeStrip'][] = 'MathJax_Parser::RemoveMathTags';
-        $wgHooks['InternalParseBeforeLinks'][] = 'MathJax_Parser::ReplaceByMarkers';
-        $wgHooks['ParserAfterTidy'][] = 'MathJax_Parser::RemoveMarkers';
-        $wgHooks['BeforePageDisplay'][] = 'MathJax_Parser::Inject_JS'; 
+
+        $wgHooks['ParserBeforeStrip'][]          = 'MathJax_Parser::RemoveMathTags';
+        $wgHooks['InternalParseBeforeLinks'][]   = 'MathJax_Parser::ReplaceByMarkers';
+        $wgHooks['ParserAfterTidy'][]            = 'MathJax_Parser::RemoveMarkers';
+        $wgHooks['BeforePageDisplay'][]          = 'MathJax_Parser::Inject_JS'; 
+
         return true;
     }
 
     static function RemoveMathTags(&$parser, &$text) 
     {
         $text = preg_replace('|:\s*<math>(.*?)</math>|s', '\\[$1\\]', $text);
-        $text = preg_replace('|<math>(.*?)</math>|s', '\\($1\\)', $text);
+        $text = preg_replace('|<math>(.*?)</math>|s',     '\\($1\\)', $text);
+
         return true;
     }
 
@@ -39,6 +43,7 @@ class MathJax_Parser {
         $tempScript = "<script type='text/javascript' src='" . $wgMathJaxJS . "?config=" . $wgMathJaxProcConf;
         if (!is_null($wgMathJaxLocConf)) $tempScript = $tempScript . "," . $wgMathJaxLocConf;
         $tempScript = $tempScript . "'> </script>";
+
         $out->addScript( $tempScript );
 
         return true;
@@ -47,10 +52,10 @@ class MathJax_Parser {
 
     static function ReplaceByMarkers(Parser &$parser, &$text ) 
     {
-        $text = preg_replace_callback('/(\$\$)(.*?)(\$\$)/s', 'MathJax_Parser::Marker', $text);
+        $text = preg_replace_callback('/(\$\$)(.*?)(\$\$)/s',                         'MathJax_Parser::Marker',$text);
         $text = preg_replace_callback('|(?<![\{\/\:\\\\])(\$)(.*?)(?<![\\\\])(\$)|s', 'MathJax_Parser::Marker', $text);
-        $text = preg_replace_callback('/(\\\\\[)(.*?)(\\\\\])/s', 'MathJax_Parser::Marker', $text);
-        $text = preg_replace_callback('/(\\\\\()(.*?)(\\\\\))/s', 'MathJax_Parser::Marker', $text);
+        $text = preg_replace_callback('/(\\\\\[)(.*?)(\\\\\])/s',                     'MathJax_Parser::Marker', $text);
+        $text = preg_replace_callback('/(\\\\\()(.*?)(\\\\\))/s',                     'MathJax_Parser::Marker', $text);
         $text = preg_replace_callback('/(\\\begin{(?:.*?)})(.*?)(\\\end{(?:.*?)})/s', 'MathJax_Parser::Marker', $text);
 
         return true;
@@ -59,17 +64,23 @@ class MathJax_Parser {
     static function NoMathJax( $text, array $args, Parser $parser, PPFrame $frame )
     {
         $output = $parser->recursiveTagParse($text, $frame);
+
         return '<span class="tex2jax_ignore">' . $output . '</span>';
     }
 
     static function RemoveMarkers( Parser &$parser, &$text )
     {
-        $text = preg_replace_callback('/' . Parser::MARKER_PREFIX . 'MathJax(?:.*?)' . Parser::MARKER_SUFFIX . '/s', 'MathJax_Parser::Test', $text);
+	/** <!-- array_values seems to return a string with "..." instead of '...' so that double backslashes (\\)
+	 *  becomes a single backslash (\) so that I do need the MarkerVal function since modifying a code
+	 *  in math mode to match these backslashes may cause unexpected behaviors. -->
+	 **/	
+	#$text = preg_replace(array_keys(self::$Markers), array_values(self::$Markers), $text);
+        $text = preg_replace_callback('/' . Parser::MARKER_PREFIX . 'MathJax(?:.*?)' . Parser::MARKER_SUFFIX . '/s', 'MathJax_Parser::MarkerVal', $text);
 
         return true;
     }
 
-    static function Test($matches)
+    static function MarkerVal($matches)
     {
         return self::$Markers[$matches[0]];
     }
@@ -78,7 +89,8 @@ class MathJax_Parser {
     static function Marker($matches)
     {
         $marker = Parser::MARKER_PREFIX . 'MathJax' . ++self::$mark_n . Parser::MARKER_SUFFIX;
-            self::$Markers[$marker] = preg_replace_callback('/' . Parser::MARKER_PREFIX . 'MathJax(?:.*?)' . Parser::MARKER_SUFFIX . '/s', 'MathJax_Parser::Test', $matches[1] . $matches[2] . $matches[3]);
+
+        self::$Markers[$marker] =  $matches[1] . $matches[2] . $matches[3];
 
         return $marker;
     }
